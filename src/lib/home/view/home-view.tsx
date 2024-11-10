f2a5fc1514b8a0d076fde7ee61eaa38b53099080
MeenS4 <rokoszoczek@gmail.com>
2024-11-10 19:30:38 +0100

    fix: add keys to list of PostCards

--- src/lib/home/view/home-view.tsx
+++ src/lib/home/view/home-view.tsx
@@ -10,16 +10,19 @@ export const HomeView = () => {
 
       <div className={styles["home-view__cards"]}>
         <PostCard
+          key={1}
           title="Monitor Dla Wymagających"
           tags={["Sprzęt", "Wyświetlacze", "4K HDR"]}
           image="post-cover.png"
         />
         <PostCard
+          key={2}
           title="Rewolucja w Świecie Klawiatur Mechanicznych"
           tags={["Komputery", "Akcesoria"]}
           image="post-cover-keyboard.png"
         />
         <PostCard
+          key={3}
           title="Czy to przyszłość? Elon Musk z Głośną Innowacją"
           tags={["Roboty", "Sztuczna Inteligencja", "Innowacja"]}
           image="post-cover-ai.png"
