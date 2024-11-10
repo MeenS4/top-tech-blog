5016200f9ceaf6bbd2b6a2ec92826cfb105b3645
MeenS4 <rokoszoczek@gmail.com>
2024-11-10 09:22:57 +0100

    feat: add post-card component

---
+++ src/lib/home/components/post-card/post-card.tsx
@@ -0,0 +1,38 @@
+import styles from "./post-card.module.scss";
+import { FontSizes, FontWeights, Text } from "@/lib/shared/components/text";
+import { PostTag } from "../post-tag";
+import { Image } from "@/lib/shared/components/image";
+
+//TODO: Change hard-coded image src to dynamic prop value
+
+export const PostCard = ({
+  title,
+  tags,
+  image,
+}: {
+  title: string;
+  tags: Array<string>;
+  image: string;
+}) => {
+  return (
+    <div className={styles["post-card"]}>
+      <Image src={image} className={styles["post-card__image"]} />
+
+      <div className={styles["post-card__content"]}>
+        <Text
+          className={styles["post-card__content__title"]}
+          size={FontSizes["24px"]}
+          weight={FontWeights.semibold}
+        >
+          {title}
+        </Text>
+
+        <div className={styles["post-card__content__tags"]}>
+          {tags.map((e) => (
+            <PostTag content={e} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/lib/home/view/home-view.tsx
+++ src/lib/home/view/home-view.tsx
@@ -1,16 +1,27 @@
 import styles from "./home-view.module.scss";
 
-import { FontSizes, FontWeights, Text } from "@/lib/shared/components/text";
-import { PostTag } from "../components/post-tag";
+import { PostCard } from "../components/post-card";
 
 export const HomeView = () => {
   return (
     <main className={styles["home-view"]}>
-      <Text size={FontSizes["20px"]} weight={FontWeights.bold}>
-        Hello world
-      </Text>
-
-      <PostTag content="Finanse" />
+      <div className={styles["home-view__cards"]}>
+        <PostCard
+          title="Monitor Dla Wymagających"
+          tags={["Sprzęt", "Wyświetlacze", "4K HDR"]}
+          image="post-cover.png"
+        />
+        <PostCard
+          title="Rewolucja w Świecie Klawiatur Mechanicznych"
+          tags={["Komputery", "Akcesoria"]}
+          image="post-cover-keyboard.png"
+        />
+        <PostCard
+          title="Czy to przyszłość? Elon Musk z Głośną Innowacją"
+          tags={["Roboty", "Sztuczna Inteligencja", "Innowacja"]}
+          image="post-cover-ai.png"
+        />
+      </div>
     </main>
   );
 };
